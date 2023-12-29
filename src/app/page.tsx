e00ba4e8799a8c305dd532bf16752d5111742755
devstefancho <skku7714@gmail.com>
2023-12-29 20:25:15 +0900

    test: vercel build with submodule test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
 import Link from "next/link";
+import path from "path";
+import fsPromise from "fs/promises";
 
-export default function Home() {
+async function getFiles() {
+  const fileListJson = await fsPromise.readFile(
+    path.join("json/file_list.json"),
+    "utf8"
+  );
+  const fileList: { path: string; name: string }[] = JSON.parse(fileListJson);
+  return fileList;
+}
+
+export default async function Home() {
+  const fileList = await getFiles();
   return (
     <div className="flex justify-center mt-10 min-h-[100vh]">
-      <Link href="/content/atomic-habits">Go to Example Content</Link>
+      {fileList.map((file) => (
+        <Link key={file.path} href={`/content/${file.name}`}>
+          Go to {file.name}
+        </Link>
+      ))}
     </div>
   );
 }
