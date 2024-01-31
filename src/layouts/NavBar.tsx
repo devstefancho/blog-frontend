457b5b2aaee7e9aa7ddd776627385be3a6292d3d
devstefancho <skku7714@gmail.com>
2024-01-31 23:26:58 +0900

    feat: backdrop filter style for header

--- src/layouts/NavBar.tsx
+++ src/layouts/NavBar.tsx
@@ -11,19 +11,32 @@ const indexName = process.env.ALGOLIA_INDEX_NAME;
 
 const NavBar: FC<PropTypes> = () => {
   return (
-    <nav className="flex w-full flex-row flex-wrap items-center justify-between pb-[16px] pl-[20px] pr-[20px] pt-[16px]">
-      <Logo />
-      <div className="flex gap-[20px]">
-        {appId && apiKey && indexName && (
-          <AlgoliaSearchBox
-            appId={appId}
-            apiKey={apiKey}
-            indexName={indexName}
-          />
-        )}
-        <ThemeButton />
-      </div>
-    </nav>
+    <header
+      className="
+      sticky top-0 z-10  border-b border-gray-300
+      bg-gray-200 bg-opacity-80 pb-[16px] pl-[20px] pr-[20px] pt-[16px]
+      backdrop-blur-[5px] backdrop-saturate-[180%] dark:border-gray-700
+      dark:bg-black dark:bg-opacity-80"
+    >
+      <nav
+        className="
+        flex w-full flex-row flex-wrap
+        items-center justify-between
+        "
+      >
+        <Logo />
+        <div className="flex gap-[20px]">
+          {appId && apiKey && indexName && (
+            <AlgoliaSearchBox
+              appId={appId}
+              apiKey={apiKey}
+              indexName={indexName}
+            />
+          )}
+          <ThemeButton />
+        </div>
+      </nav>
+    </header>
   );
 };
 
